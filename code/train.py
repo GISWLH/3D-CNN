6d92318486aac83e79ba4030f14933a8231a4cdd
GISWLH <hhuwlh@163.com>
2024-12-30 20:20:02 +0800

    train

--- code/train.py
+++ code/train.py
@@ -1,28 +1,31 @@
 import torch
 import torch.nn as nn
+import os
 
+print(os.path.exists('../../../data/model_checkpoints'))
+print(os.path.exists('../../../data'))
 class CNN3D(nn.Module):
     def __init__(self, input_features, output_features, target_time_steps):
         super(CNN3D, self).__init__()
         # First layer: extracting spatial and temporal features
         self.conv1 = nn.Conv3d(
             in_channels=input_features,
-            out_channels=64,
+            out_channels=16,
             kernel_size=(3, 3, 3),
             padding=(1, 1, 1)
         )
         
         # Second layer: further extract deep features
         self.conv2 = nn.Conv3d(
-            in_channels=64,
-            out_channels=128,
+            in_channels=16,
+            out_channels=32,
             kernel_size=(3, 3, 3),
             padding=(1, 1, 1)
         )
         
         # Third layer: reduce the time step from 16 to 3, and the feature dimension is 1
         self.conv3 = nn.Conv3d(
-            in_channels=128,
+            in_channels=32,
             out_channels=output_features,
             kernel_size=(3, 1, 1),
             stride=(5, 1, 1),
@@ -43,7 +46,7 @@ class CNN3D(nn.Module):
         x = self.conv3(x)  # (1, 128, 16, 721, 1440) -> (1, 1, 3, 721, 1440)
         return x
     
-normalized_data = torch.load('E:/data/normalized_train.pt')
+normalized_data = torch.load('../../../data/normalized_train.pt')
 from torch.utils.data import Dataset, DataLoader
 from random import randint
 import torch
@@ -76,17 +79,18 @@ class WeatherDataset1(Dataset):
         target_surface = self.input[idx + 16: idx + 16 + 3, 0, :, :]
         return upper_input, target_surface
     
-DEVICE = torch.device("cuda:0")
-model = CNN3D(input_features=8, output_features=1, target_time_steps=16).to(DEVICE)
-
+DEVICE = torch.device("cuda")
+model = CNN3D(input_features=8, output_features=1, target_time_steps=16)
+model = nn.DataParallel(model)
+model.to(DEVICE)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
 
 # 创建数据集和数据加载器
 train_dataset = WeatherDataset1(normalized_data, train=True)
-train_loader = DataLoader(train_dataset, batch_size=1,shuffle=False, generator=torch.Generator(device='cpu'))
+train_loader = DataLoader(train_dataset, batch_size=2,shuffle=False, generator=torch.Generator(device='cpu'))
 
 valid_dataset = WeatherDataset1(normalized_data, validation=True)
-valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False)
+valid_loader = DataLoader(valid_dataset, batch_size=2, shuffle=False)
 
 custom_loss = torch.nn.MSELoss()
 epochs = 100
@@ -98,9 +102,8 @@ for epoch in range(epochs):
         upper_air, target_surface = upper_air.to(DEVICE), target_surface.to(DEVICE)
         optimizer.zero_grad()  # 清除旧的梯度
         output_surface = model(upper_air)
-        
-        output_surface = output_surface.squeeze(0).squeeze(0)
-        target_surface = target_surface.squeeze(0).squeeze(0)
+        output_surface = output_surface.squeeze(1)
+        target_surface = target_surface
 
         loss = custom_loss(output_surface, target_surface)
 
@@ -128,8 +131,8 @@ for epoch in range(epochs):
     print(f"Epoch {epoch}, Train Loss: {train_loss}, Validation Loss: {valid_loss}")
     with open(log_file, "a") as f:
         f.write(f"{epoch},{train_loss},{valid_loss}\n")
-    if (epoch + 1) % 20 == 0:
-        checkpoint_path = f'model_checkpoints/model_epoch_{epoch+1}.pth'
+    if (epoch + 1) % 10 == 0:
+        checkpoint_path = f'../../../data/model_checkpoints/model_epoch_{epoch+1}.pth'
         torch.save({
             'epoch': epoch + 1,
             'model_state_dict': model.state_dict(),
@@ -139,4 +142,4 @@ for epoch in range(epochs):
         }, checkpoint_path)
         print(f"Model checkpoint saved at epoch {epoch + 1}")
         
-torch.save(model, '3DCNN_0005_100ep.pth')
+torch.save(model, '../../../data/3DCNN_0005_100ep.pth')
